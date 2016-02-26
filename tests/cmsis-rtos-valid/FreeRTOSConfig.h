964d3aced2a77c2aba9e772291f696d54c44a067
Liviu Ionescu <ilg@livius.net>
2016-02-26 19:07:11 +0200

    validation test: increase rtos heap to 40KB

--- tests/cmsis-rtos-valid/FreeRTOSConfig.h
+++ tests/cmsis-rtos-valid/FreeRTOSConfig.h
@@ -103,7 +103,7 @@ extern uint32_t SystemCoreClock;
 #define configTICK_RATE_HZ						( OS_INTEGER_SYSTICK_FREQUENCY_HZ )
 #define configMAX_PRIORITIES					( 32 )
 #define configMINIMAL_STACK_SIZE				( ( unsigned short ) 256 )
-#define configTOTAL_HEAP_SIZE					( ( size_t ) ( 10 * 1024 ) )
+#define configTOTAL_HEAP_SIZE					( ( size_t ) ( 40 * 1024 ) )
 #define configMAX_TASK_NAME_LEN					( 10 )
 #define configUSE_TRACE_FACILITY				1
 #define configUSE_16_BIT_TICKS					0
